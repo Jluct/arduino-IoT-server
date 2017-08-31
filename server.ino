ce029bb13260de3fb70d669e684af773e113247f
Jluct <panaev02@gmail.com>
2017-08-31 18:11:04 +0300

    sheet

--- server.ino
+++ server.ino
@@ -1,6 +1,6 @@
 /*
   Web Server
-  
+
   created 18 Dec 2009
   by David A. Mellis
   modified 9 Apr 2012
@@ -10,13 +10,125 @@
 
   modified 30 Aug 2017
   by Listopadov Sergei
- 
+
 */
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <OneWire.h>
 
-int sensorsPin[] = {1, 2, 3, 4};
+/////SENSOR FUNCTIONS///////
+
+int getTemperature(int pin) {
+  OneWire  ds(pin);
+
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius;
+
+  if ( !ds.search(addr)) {
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+
+  //  Serial.print("ROM =");
+//  for ( i = 0; i < 8; i++) {
+//    //    Serial.write(' ');
+//    //    Serial.print(addr[i], HEX);
+//  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    Serial.println("CRC is not valid!");
+    return;
+  }
+  //  Serial.println();
+
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      //      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      //      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      //      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      //      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44);        // start conversion, with parasite power on at the end
+
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);         // Read Scratchpad
+
+  //  Serial.print("  Data = ");
+  //  Serial.print(present, HEX);
+  //  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    //    Serial.print(data[i], HEX);
+    //    Serial.print(" ");
+  }
+  //  Serial.print(" CRC=");
+  //  Serial.print(OneWire::crc8(data, 8), HEX);
+  //  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  //  Serial.print("  Temperature = ");
+  //  Serial.println(celsius);
+  //  Serial.print(" Celsius, ");
+  //  Serial.print(fahrenheit);
+  //  Serial.println(" Fahrenheit");
+  Serial.println(celsius);
+  return (int)celsius;
+
+}
+
+int getLightData(int pin) {
+  pinMode(pin, INPUT);
+  return map(analogRead(pin), 0, 1024, 0, 100);
+}
+
+
+int sensorsPin[] = {10, A0};
+
+int (*funcArray[10])(char * str, int len) = {getTemperature, getLightData};
+
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -37,6 +149,7 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
 
   }
+
   // Serial.println(generateSensorsToJSON()); //from debug
 
   // start the Ethernet connection and the server:
@@ -69,7 +182,7 @@ void loop() {
           client.println("Connection: close");  // the connection will be closed after completion of the response
           //          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
-          
+
           client.print(generateSensorsToJSON());
 
           //          client.println("</html>");
@@ -102,13 +215,21 @@ String generateSensorsToJSON() {
     out.concat(itoa(sensorsPin[i], stringBuffer, 10));
     out.concat("\"");
     out.concat(":");
-    out.concat(itoa(random(10, 20), stringBuffer, 10));
+    out.concat(itoa(((funcArray[i])(sensorsPin[i])), stringBuffer, 10));
     if ((sizeof(sensorsPin) / sizeof(int)) - 1 != i)
       out.concat(",");
   }
-  
+
   out.concat("}");
   Serial.println(out);
   return out;
 }
+
+
+
+
+
+
+
+
 
